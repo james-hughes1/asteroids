f9b7c8399f74e499591edce8544fc384581452ac
James Hughes <jhughes2712@gmail.com>
2025-10-25 13:12:02 +0100

    Change: Noisy nets.

--- src/utils/model_io.py
+++ src/utils/model_io.py
@@ -1,6 +1,6 @@
 import os
 import torch
-from training.dqn_model import DuelingDQN
+from training.dqn_model import NoisyDQN
 
 def save_model(model, config, n_actions, save_path):
     """
@@ -51,9 +51,9 @@ def load_model(model_path, device="cpu"):
     n_actions = checkpoint["n_actions"]
 
     # Recreate model and load weights
-    model = DuelingDQN(input_shape, n_actions).to(device)
+    model = NoisyDQN(input_shape, n_actions).to(device)
     model.load_state_dict(checkpoint["state_dict"])
-    #model.reset_noise()  # important for NoisyLinear layers
+    model.reset_noise()  # important for NoisyLinear layers
     model.eval()
 
     print(f"Loaded model from {model_path}")
